63af159dc2fd9dad30974922b2c8916044645651
İbrahim <ibrahimtanrikulu38@hotmail.com>
2024-12-02 05:25:59 +0300

    test 17

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -22,7 +22,7 @@ export class AuthGuard implements CanActivate {
       this.router.navigate(['/login'], {
         queryParams: { returnUrl: state.url },
       });
-      return false;
+      return true;
     }
     return true;
   }
